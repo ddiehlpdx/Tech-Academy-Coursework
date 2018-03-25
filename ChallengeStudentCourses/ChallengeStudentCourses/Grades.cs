25fe0583b9daab48d96bb06f306d2e1260f7f6f9
Dave Diehl <ddiehl.pdx@gmail.com>
2018-03-25 11:18:20 -0700

    Commit student courses challenge.

---
+++ ChallengeStudentCourses/ChallengeStudentCourses/Grades.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChallengeStudentCourses {
+	public class Grades {
+		public int CourseId { get; set; }
+		public int StudentId { get; set; }
+		public int Grade { get; set; }
+		public List<Student> Students { get; set; }
+	}
+}
