1b657ea73168012c5d36780941354bcf4105b73e
Dave Diehl <ddiehl.pdx@gmail.com>
2018-04-04 17:49:07 -0700

    Commit first MVC challenge.
    First challenge and quick entity test.

---
+++ C-Sharp/MVC/Guitars/Guitars/Controllers/GuitarsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Guitars.Models;
+
+namespace Guitars.Controllers
+{
+    public class GuitarsController : Controller
+    {
+        private GuitarsEntities db = new GuitarsEntities();
+
+        // GET: Guitars
+        public ActionResult Index()
+        {
+            return View(db.Guitars.ToList());
+        }
+
+        // GET: Guitars/Details/5
+        public ActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Guitar guitar = db.Guitars.Find(id);
+            if (guitar == null)
+            {
+                return HttpNotFound();
+            }
+            return View(guitar);
+        }
+
+        // GET: Guitars/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Guitars/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Brand,Model,Price")] Guitar guitar)
+        {
+            if (ModelState.IsValid)
+            {
+                guitar.Id = Guid.NewGuid();
+                db.Guitars.Add(guitar);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(guitar);
+        }
+
+        // GET: Guitars/Edit/5
+        public ActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Guitar guitar = db.Guitars.Find(id);
+            if (guitar == null)
+            {
+                return HttpNotFound();
+            }
+            return View(guitar);
+        }
+
+        // POST: Guitars/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Brand,Model,Price")] Guitar guitar)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(guitar).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(guitar);
+        }
+
+        // GET: Guitars/Delete/5
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Guitar guitar = db.Guitars.Find(id);
+            if (guitar == null)
+            {
+                return HttpNotFound();
+            }
+            return View(guitar);
+        }
+
+        // POST: Guitars/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            Guitar guitar = db.Guitars.Find(id);
+            db.Guitars.Remove(guitar);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ C-Sharp/MVC/Guitars/Guitars/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Guitars.MvcApplication" Language="C#" %>
