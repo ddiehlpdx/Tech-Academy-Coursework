d7ba125b98dbcb2cd3d0e8769a0ea64d391431a2
Dave Diehl <ddiehl.pdx@gmail.com>
2018-04-11 20:10:42 -0700

    Commit simple SPA demo.

---
+++ C-Sharp/MVC/SPADemo/SPADemo/Models/ViewModel.js
@@ -0,0 +1,15 @@
+﻿
+var AppViewModel = function () {
+    var self = this;
+    self.random = ko.observable("");
+    self.noun = ko.observable("");
+    self.verb = ko.observable("");
+    self.adjective = ko.observable("");
+    self.name = ko.observable("");    
+    self.clicked = ko.observable(false);
+
+    self.showLib = function () {
+        self.clicked(!self.clicked());
+    };
+}
+ko.applyBindings(new AppViewModel());
